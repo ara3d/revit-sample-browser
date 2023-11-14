e92366f20a7c2582796e7676dd26aa75f70c9f51
Christopher Diggins <cdiggins@gmail.com>
2023-11-14 00:09:54 -0500

    RTF files

--- utils/MultiSampleDevUtils/UnitTest1.cs
+++ utils/MultiSampleDevUtils/UnitTest1.cs
@@ -2,7 +2,7 @@ using RevitMultiSample;
 
 namespace MultiSampleDevUtils
 {
-    public class Tests
+    public class Tools
     {
         [Test, Explicit]
         public static void TestUI()
@@ -15,6 +15,16 @@ namespace MultiSampleDevUtils
                 File.WriteAllLines(f, contents
                     .SkipWhile(s => s.StartsWith("//"))
                     .Prepend("// Copyright 2023. See https://github.com/ara3d/revit-samples/LICENSE.txt"));
+            }
+        }
+
+        [Test, Explicit]
+        public static void FindReadmeFiles()
+        {
+            var path = @"C:\Users\cdigg\git\revit-samples\src";
+            foreach (var f in Directory.GetFiles(path, "*.rtf", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(f);
             }
         }
     }
